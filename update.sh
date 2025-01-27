618fa5325cbc72e122d0ea460062178929500a7a
jinqiang zhang <jinqiang@zhang.my>
2025-01-27 09:00:48 +0000

    init

---
+++ update.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+# commit
+if [[ $(git diff --stat) != '' ]]; then
+  git config --local user.email "github-actions[bot]@users.noreply.github.com"
+  git config --local user.name "github-actions[bot]"
+  pushd ./dnsmasq-china-list
+  make
+  popd
+  cp -v ./dnsmasq-china-list/*.raw.txt .
+  git add .
+  git commit -m "$(TZ=Asia/Shanghai date +%Y-%m-%dT%H:%M:%S%z) update"
+  git push
+fi
